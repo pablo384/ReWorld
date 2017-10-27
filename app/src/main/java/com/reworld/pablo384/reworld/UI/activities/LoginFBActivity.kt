856b00562d08ddb0f2f47fc1e0cb9690899e10f4
Pablo Luis Reinoso <pabloreinoso0@gmail.com>
2017-10-27 19:40:38 -0400

    Login con Firebase y Facebook implementado

--- app/src/main/java/com/reworld/pablo384/reworld/UI/activities/LoginFBActivity.kt
+++ app/src/main/java/com/reworld/pablo384/reworld/UI/activities/LoginFBActivity.kt
@@ -8,13 +8,25 @@ import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.reworld.pablo384.reworld.R
 import kotlinx.android.synthetic.main.activity_login_fb.*
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
 import com.facebook.login.LoginResult
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+import android.util.Log
+import com.google.firebase.auth.FacebookAuthProvider
+import com.facebook.AccessToken
+import org.jetbrains.anko.toast
 
 
 class LoginFBActivity : AppCompatActivity() {
     var callbackManager:CallbackManager?=null
+    private var mAuth: FirebaseAuth? = null
+
+    companion object {
+        val TAG = "LoginFBActivity"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +34,7 @@ class LoginFBActivity : AppCompatActivity() {
         AppEventsLogger.activateApp(baseContext)
         setContentView(R.layout.activity_login_fb)
 
+        mAuth = FirebaseAuth.getInstance()
 
 
         callbackManager = CallbackManager.Factory.create()
@@ -34,7 +47,8 @@ class LoginFBActivity : AppCompatActivity() {
 
             }
 
-            override fun onSuccess(result: LoginResult?) {
+            override fun onSuccess(result: LoginResult) {
+                handleFacebookAccessToken(result.accessToken)
 
             }
 
@@ -48,5 +62,41 @@ class LoginFBActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         callbackManager?.onActivityResult(requestCode,resultCode,data)
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = mAuth?.currentUser
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(LoginFBActivity.TAG, "handleFacebookAccessToken:" + token)
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        mAuth?.signInWithCredential(credential)
+                ?.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(LoginFBActivity.TAG, "signInWithCredential:success")
+                        val user = mAuth?.currentUser
+                        toast("welcome ${user?.displayName}")
+                        procedApplication(MainActivity::class.java)
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(LoginFBActivity.TAG, "signInWithCredential:failure", task.getException())
+                        Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+
+                    }
+
+                    // ...
+                }
+    }
+    private fun procedApplication(clase: Class<*>) {
+        val intent = Intent(this@LoginFBActivity, clase)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP and Intent.FLAG_ACTIVITY_CLEAR_TASK and Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+        finish()
     }
 }
